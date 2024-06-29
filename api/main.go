7e60d45c849d6c7c81784ff66ccfe8d5dcc4ac9f
gzttcydxx <17377253@buaa.edu.cn>
2024-06-29 23:06:21 +0800

    fix CORS problem

--- api/main.go
+++ api/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"strings"
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/gzttcydxx/api/gateway"
 	"github.com/gzttcydxx/api/route"
@@ -20,6 +24,33 @@ func main() {
 	r := gin.Default()
 	// 设置信任的代理
 	// r.SetTrustedProxies([]string{"traefik"})
+
+	// 使用 CORS 中间件处理跨域问题，配置 CORS 参数
+	r.Use(cors.New(cors.Config{
+		// 允许的源地址（CORS中的Access-Control-Allow-Origin）
+		// AllowOrigins: []string{"https://foo.com"},
+		// 允许的 HTTP 方法（CORS中的Access-Control-Allow-Methods）
+		AllowMethods: []string{"GET", "POST", "PUT", "PATCH", "DELETE"},
+		// 允许的 HTTP 头部（CORS中的Access-Control-Allow-Headers）
+		AllowHeaders: []string{"Origin"},
+		// 暴露的 HTTP 头部（CORS中的Access-Control-Expose-Headers）
+		ExposeHeaders: []string{"Content-Length"},
+		// 是否允许携带身份凭证（CORS中的Access-Control-Allow-Credentials）
+		AllowCredentials: true,
+		// 允许源的自定义判断函数，返回true表示允许，false表示不允许
+		AllowOriginFunc: func(origin string) bool {
+			if strings.HasPrefix(origin, "http://localhost") {
+				// 允许你的开发环境
+				return true
+			} else if strings.HasPrefix(origin, "http://127.0.0.1") {
+				return true
+			}
+			// 允许包含 "yourcompany.com" 的源
+			return strings.Contains(origin, "a.gzttc.top")
+		},
+		// 用于缓存预检请求结果的最大时间（CORS中的Access-Control-Max-Age）
+		MaxAge: 12 * time.Hour,
+	}))
 
 	r.GET("/", route.HandleRoot)
 
